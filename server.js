4d3e2341dd9407d325ea0f10d397dbc798d164dc
Alberto Naranjo Moya <boscoalbertosusana@gmail.com>
2024-05-11 03:05:26 +0200

    first commit

--- server.js
+++ server.js
@@ -22,5 +22,5 @@ wss.on('connection', ws => {
     });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = 3000;
 server.listen(PORT, () => console.log(`Servidor WebSocket en ejecución en el puerto ${PORT}.`));
