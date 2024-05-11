5914f5707427af3230351e5be91218da3934b1ee
Alberto Naranjo Moya <boscoalbertosusana@gmail.com>
2024-05-11 04:14:21 +0200

    1.0.9

--- public/script.js
+++ public/script.js
@@ -18,13 +18,13 @@ socket.addEventListener('message', event => {
     appendMessage(`${message.username}: ${message.message}`);
 });
 
-sendButton.addEventListener('click', () => {
-    const message = messageInput.value;
-    if (message && socket.readyState === WebSocket.OPEN) {
-        sendMessage(message);
-        messageInput.value = '';
+socket.addEventListener('message', event => {
+    const receivedMessage = JSON.parse(event.data);
+    if (receivedMessage.username !== username) { // Verificar si el mensaje recibido no proviene del usuario actual
+        appendMessage(`${receivedMessage.username}: ${receivedMessage.message}`);
     }
 });
+
 
 function sendMessage(message) {
     const messageToSend = JSON.stringify({ username, message });
