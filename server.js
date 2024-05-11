aa81075a7f9b25d30ca6177ae9c83ff9ab7efb0a
Alberto Naranjo Moya <boscoalbertosusana@gmail.com>
2024-05-11 04:05:17 +0200

    1.0.8

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ wss.on('connection', ws => {
     console.log('Cliente conectado.');
 
     ws.on('message', message => {
-        console.log(`Mensaje recibido: ${message}`);
+        const parsedMessage = JSON.parse(message);
+        console.log(`Mensaje recibido de ${parsedMessage.username}: ${parsedMessage.message}`);
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
                 client.send(message);
@@ -20,6 +21,7 @@ wss.on('connection', ws => {
         });
     });
 });
+
 
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Servidor WebSocket en ejecución en el puerto ${PORT}.`));
